b6007b8e2372bfcee3097cb63387f23d197770aa
darlandieterich <darlandieterich@gmail.com>
2020-12-07 23:50:42 -0300

    Running project

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,12 @@
 export default {
   name: 'App',
   components: {
-    RetroConsole
   },
   data () {
     return {
-      lines: ['one', 'two', 'three'],
-      value: ''
     }
   },
   methods: {
-    insere () {
-      this.lines.push(this.value)
-      this.value = ''
-    }
   }
 }
 </script>
