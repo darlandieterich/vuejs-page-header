dfef3ed50aef5e054ef92ccf29883a3da765db34
darlandieterich <darlandieterich@gmail.com>
2020-12-07 23:35:22 -0300

    Added structural files

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    ..
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    RetroConsole
+  },
+  data () {
+    return {
+      lines: ['one', 'two', 'three'],
+      value: ''
+    }
+  },
+  methods: {
+    insere () {
+      this.lines.push(this.value)
+      this.value = ''
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+  padding: 0px;
+}
+</style>
---
+++ src/PageHeader/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'PageHeader',
+  props: {
+  },
+  computed: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
